5ac471072fc25bcdeefce6c9df17904d0476f64d
Mantaine <lucytorinke@gmail.com>
2023-07-23 11:17:49 -0700

    printf.c

--- _printf.c
+++ _printf.c
@@ -12,7 +12,7 @@ int _printf(const char *format, ...)
 int i, printed = 0, printed_chars = 0;
 int flags, width, precision, size, buff_ind = 0;
 va_list list;
-char buffer[BUFF _SIZE];
+char buffer[BUFF_SIZE];
 
 if (format == NULL)
 return (-1);
@@ -35,7 +35,8 @@ width = get_width(format, &i, list);
 precision = get_precision(format, &i, list);
 size = get_size(format, &i);
 ++i;
-printed = handle_print(format, &i, list,buffer,flags, width, precision, size);
+printed = handle_print(format, &i, list,
+		buffer, flags, width, precision, size);
 if (printed == -1)
 return (-1);
 printed_chars += printed;
